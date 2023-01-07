b44f1372aef6f3d372995406bffc02fb4649d024
Str0-codes <danielMMomodu@gmail.com>
2023-01-07 15:29:04 +0100

    Update

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,16 +1,16 @@
 function getCurrentYear () {
-  const date = new Date()
-  return date.getFullYear()
+	const date = new Date();
+	return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear (income, gdp, capita) {
-  const budget = {
+	const budget = {
 
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita
-  }
-  return budget
+		[`income-${getCurrentYear()}`]: income,
+		[`gdp-${getCurrentYear()}`]: gdp,
+		[`capita-${getCurrentYear()}`]: capita
+	};
+	return budget;
 }
 
-console.log(getBudgetForCurrentYear(2100, 5200, 1090))
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
