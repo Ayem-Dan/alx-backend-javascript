1f4509ea83dd9f8a3d56e79041981e33f7215c8a
Ayem-Dan <danielMMomodu@gmail.com>
2023-02-24 00:15:37 +0100

    Update

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let itemstr = '';
-  let i = 0;
-  for (const item of reportWithIterator) {
-    if (reportWithIterator.length - 1 === i) {
-      itemstr += item;
-    } else {
-      itemstr += `${item} |`;
-    }
-    i += i;
-  }
-  return itemstr;
+  return reportWithIterator.join(' | ');
 }
